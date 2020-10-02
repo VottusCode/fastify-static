3869bfc287e6bb3d614ac815eb247786268e3a04
VottusCode <vottus@vott.us>
2020-10-02 23:44:51 +0200

    lol

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const url = require('url')
-const statSync = require('fs').statSync
-const { PassThrough } = require('readable-stream')
-const glob = require('glob')
-const send = require('send')
-const fp = require('fastify-plugin')
+const path = require("path");
+const url = require("url");
+const statSync = require("fs").statSync;
+const { PassThrough } = require("readable-stream");
+const glob = require("glob");
+const send = require("send");
+const fp = require("fastify-plugin");
+const fs = require("fs");
 
-const dirList = require('./lib/dirList')
+const dirList = require("./lib/dirList");
 
-function fastifyStatic (fastify, opts, next) {
-  const error = checkRootPathForErrors(fastify, opts.root)
-  if (error !== undefined) return next(error)
+function fastifyStatic(fastify, opts, next) {
+  const error = checkRootPathForErrors(fastify, opts.root);
+  if (error !== undefined) return next(error);
 
-  const setHeaders = opts.setHeaders
+  const setHeaders = opts.setHeaders;
 
-  if (setHeaders !== undefined && typeof setHeaders !== 'function') {
-    return next(new TypeError('The `setHeaders` option must be a function'))
+  if (setHeaders !== undefined && typeof setHeaders !== "function") {
+    return next(new TypeError("The `setHeaders` option must be a function"));
   }
 
-  const invalidDirListOpts = dirList.validateOptions(opts.list)
+  const invalidDirListOpts = dirList.validateOptions(opts.list);
   if (invalidDirListOpts) {
-    return next(invalidDirListOpts)
+    return next(invalidDirListOpts);
   }
 
   const sendOptions = {
@@ -35,195 +36,227 @@ function fastifyStatic (fastify, opts, next) {
     immutable: opts.immutable,
     index: opts.index,
     lastModified: opts.lastModified,
-    maxAge: opts.maxAge
-  }
+    maxAge: opts.maxAge,
+  };
 
-  function pumpSendToReply (request, reply, pathname, rootPath) {
-    var options = Object.assign({}, sendOptions)
+  function pumpSendToReply(request, reply, pathname, rootPath) {
+    var options = Object.assign({}, sendOptions);
 
     if (rootPath) {
-      options.root = rootPath
+      options.root = rootPath;
     }
 
-    const stream = send(request.raw, pathname, options)
-    var resolvedFilename
-    stream.on('file', function (file) {
-      resolvedFilename = file
-    })
+    const stream = send(request.raw, pathname, options);
+    var resolvedFilename;
+    stream.on("file", function (file) {
+      resolvedFilename = file;
+    });
 
     const wrap = new PassThrough({
-      flush (cb) {
-        this.finished = true
+      flush(cb) {
+        this.finished = true;
         if (reply.raw.statusCode === 304) {
-          reply.send('')
+          reply.send("");
         }
-        cb()
-      }
-    })
-
-    wrap.getHeader = reply.getHeader.bind(reply)
-    wrap.setHeader = reply.header.bind(reply)
-    wrap.socket = request.raw.socket
-    wrap.finished = false
-
-    Object.defineProperty(wrap, 'filename', {
-      get () {
-        return resolvedFilename
-      }
-    })
-    Object.defineProperty(wrap, 'statusCode', {
-      get () {
-        return reply.raw.statusCode
+        cb();
       },
-      set (code) {
-        reply.code(code)
-      }
-    })
+    });
 
-    wrap.on('pipe', function () {
-      reply.send(wrap)
-    })
+    wrap.getHeader = reply.getHeader.bind(reply);
+    wrap.setHeader = reply.header.bind(reply);
+    wrap.socket = request.raw.socket;
+    wrap.finished = false;
+
+    Object.defineProperty(wrap, "filename", {
+      get() {
+        return resolvedFilename;
+      },
+    });
+    Object.defineProperty(wrap, "statusCode", {
+      get() {
+        return reply.raw.statusCode;
+      },
+      set(code) {
+        reply.code(code);
+      },
+    });
+
+    wrap.on("pipe", function () {
+      reply.send(wrap);
+    });
 
     if (setHeaders !== undefined) {
-      stream.on('headers', setHeaders)
+      stream.on("headers", setHeaders);
     }
 
-    stream.on('directory', function (_, path) {
+    stream.on("directory", function (_, path) {
       if (opts.list) {
-        return dirList.send({ reply, dir: path, options: opts.list, route: pathname })
+        return dirList.send({
+          reply,
+          dir: path,
+          options: opts.list,
+          route: pathname,
+        });
       }
 
       if (opts.redirect === true) {
         /* eslint node/no-deprecated-api: "off" */
-        const parsed = url.parse(request.raw.url)
-        reply.redirect(301, parsed.pathname + '/' + (parsed.search || ''))
+        const parsed = url.parse(request.raw.url);
+        reply.redirect(301, parsed.pathname + "/" + (parsed.search || ""));
       } else {
-        reply.callNotFound()
+        reply.callNotFound();
       }
-    })
+    });
 
-    stream.on('error', function (err) {
+    stream.on("error", function (err) {
       if (err) {
-        if (err.code === 'ENOENT') {
+        if (err.code === "ENOENT") {
           // if file exists, send real file, otherwise send dir list if name match
           if (opts.list && dirList.handle(pathname, opts.list)) {
-            return dirList.send({ reply, dir: dirList.path(opts.root, pathname), options: opts.list, route: pathname })
+            return dirList.send({
+              reply,
+              dir: dirList.path(opts.root, pathname),
+              options: opts.list,
+              route: pathname,
+            });
           }
-          return reply.callNotFound()
+          console.log("err    ", path.join(options.root, "index.html"));
+          return reply
+            .type("text/html")
+            .send(fs.createReadStream(path.join(options.root, "index.html")));
         }
-        reply.send(err)
+        reply.send(err);
       }
-    })
+    });
 
     // we cannot use pump, because send error
     // handling is not compatible
-    stream.pipe(wrap)
+    stream.pipe(wrap);
   }
 
-  if (opts.prefix === undefined) opts.prefix = '/'
+  if (opts.prefix === undefined) opts.prefix = "/";
 
-  let prefix = opts.prefix
+  let prefix = opts.prefix;
 
   if (!opts.prefixAvoidTrailingSlash) {
-    prefix = opts.prefix[opts.prefix.length - 1] === '/' ? opts.prefix : (opts.prefix + '/')
+    prefix =
+      opts.prefix[opts.prefix.length - 1] === "/"
+        ? opts.prefix
+        : opts.prefix + "/";
   }
 
   // Set the schema hide property if defined in opts or true by default
-  const schema = { schema: { hide: typeof opts.schemaHide !== 'undefined' ? opts.schemaHide : true } }
+  const schema = {
+    schema: {
+      hide: typeof opts.schemaHide !== "undefined" ? opts.schemaHide : true,
+    },
+  };
 
   if (opts.decorateReply !== false) {
-    fastify.decorateReply('sendFile', function (filePath, rootPath) {
-      pumpSendToReply(this.request, this, filePath, rootPath)
-      return this
-    })
+    fastify.decorateReply("sendFile", function (filePath, rootPath) {
+      pumpSendToReply(this.request, this, filePath, rootPath);
+      return this;
+    });
   }
 
   if (opts.serve !== false) {
     if (opts.wildcard === undefined || opts.wildcard === true) {
-      fastify.get(prefix + '*', schema, function (req, reply) {
-        pumpSendToReply(req, reply, '/' + req.params['*'])
-      })
+      fastify.get(prefix + "*", schema, function (req, reply) {
+        pumpSendToReply(req, reply, "/" + req.params["*"]);
+      });
       if (opts.redirect === true && prefix !== opts.prefix) {
         fastify.get(opts.prefix, schema, function (req, reply) {
           /* eslint node/no-deprecated-api: "off" */
-          const parsed = url.parse(req.raw.url)
-          reply.redirect(301, parsed.pathname + '/' + (parsed.search || ''))
-        })
+          const parsed = url.parse(req.raw.url);
+          reply.redirect(301, parsed.pathname + "/" + (parsed.search || ""));
+        });
       }
     } else {
-      const globPattern = typeof opts.wildcard === 'string' ? opts.wildcard : '**/*'
-      glob(path.join(sendOptions.root, globPattern), { nodir: true }, function (err, files) {
+      const globPattern =
+        typeof opts.wildcard === "string" ? opts.wildcard : "**/*";
+      glob(path.join(sendOptions.root, globPattern), { nodir: true }, function (
+        err,
+        files
+      ) {
         if (err) {
-          return next(err)
+          return next(err);
         }
-        const indexDirs = new Set()
-        const indexes = typeof opts.index === 'undefined' ? ['index.html'] : [].concat(opts.index || [])
+        const indexDirs = new Set();
+        const indexes =
+          typeof opts.index === "undefined"
+            ? ["index.html"]
+            : [].concat(opts.index || []);
         for (let file of files) {
-          file = file.replace(sendOptions.root.replace(/\\/g, '/'), '').replace(/^\//, '')
-          const route = (prefix + file).replace(/\/\//g, '/')
+          file = file
+            .replace(sendOptions.root.replace(/\\/g, "/"), "")
+            .replace(/^\//, "");
+          const route = (prefix + file).replace(/\/\//g, "/");
           fastify.get(route, schema, function (req, reply) {
-            pumpSendToReply(req, reply, '/' + file)
-          })
+            pumpSendToReply(req, reply, "/" + file);
+          });
 
           if (indexes.includes(path.posix.basename(route))) {
-            indexDirs.add(path.posix.dirname(route))
+            indexDirs.add(path.posix.dirname(route));
           }
         }
         indexDirs.forEach(function (dirname) {
-          const pathname = dirname + (dirname.endsWith('/') ? '' : '/')
-          const file = '/' + pathname.replace(prefix, '')
+          const pathname = dirname + (dirname.endsWith("/") ? "" : "/");
+          const file = "/" + pathname.replace(prefix, "");
 
           fastify.get(pathname, schema, function (req, reply) {
-            pumpSendToReply(req, reply, file)
-          })
+            pumpSendToReply(req, reply, file);
+          });
 
           if (opts.redirect === true) {
-            fastify.get(pathname.replace(/\/$/, ''), schema, function (req, reply) {
-              pumpSendToReply(req, reply, file.replace(/\/$/, ''))
-            })
+            fastify.get(pathname.replace(/\/$/, ""), schema, function (
+              req,
+              reply
+            ) {
+              pumpSendToReply(req, reply, file.replace(/\/$/, ""));
+            });
           }
-        })
-        next()
-      })
+        });
+        next();
+      });
 
       // return early to avoid calling next afterwards
-      return
+      return;
     }
   }
 
-  next()
+  next();
 }
 
-function checkRootPathForErrors (fastify, rootPath) {
+function checkRootPathForErrors(fastify, rootPath) {
   if (rootPath === undefined) {
-    return new Error('"root" option is required')
+    return new Error('"root" option is required');
   }
-  if (typeof rootPath !== 'string') {
-    return new Error('"root" option must be a string')
+  if (typeof rootPath !== "string") {
+    return new Error('"root" option must be a string');
   }
   if (path.isAbsolute(rootPath) === false) {
-    return new Error('"root" option must be an absolute path')
+    return new Error('"root" option must be an absolute path');
   }
 
-  var pathStat
+  var pathStat;
 
   try {
-    pathStat = statSync(rootPath)
+    pathStat = statSync(rootPath);
   } catch (e) {
-    if (e.code === 'ENOENT') {
-      fastify.log.warn(`"root" path "${rootPath}" must exist`)
-      return
+    if (e.code === "ENOENT") {
+      fastify.log.warn(`"root" path "${rootPath}" must exist`);
+      return;
     }
 
-    return e
+    return e;
   }
 
   if (pathStat.isDirectory() === false) {
-    return new Error('"root" option must point to a directory')
+    return new Error('"root" option must point to a directory');
   }
 }
 
 module.exports = fp(fastifyStatic, {
-  fastify: '3.x',
-  name: 'fastify-static'
-})
+  fastify: "3.x",
+  name: "fastify-static",
+});
